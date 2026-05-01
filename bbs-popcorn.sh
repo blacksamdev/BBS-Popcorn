a37f596d3ad7fed8dddd022c0b625446e4feb039
blacksamdev <bellamymichael01@gmail.com>
2026-05-01 10:51:46 +0200

    upodate SponsorBlock

--- bbs-popcorn.sh
+++ bbs-popcorn.sh
@@ -1,7 +1,16 @@
-#!/bin/sh
-set -eu
+#!/bin/bash
+
+set -e
 
 export PYTHONUNBUFFERED=1
 export PYTHONPATH=/app/lib/bbs-popcorn
 
-exec python3 -m bbs_popcorn.main
+# --- Inject sponsorblock dans le mpv Flatpak ---
+MPV_CONFIG_DIR="$HOME/.var/app/io.mpv.Mpv/config/mpv/scripts"
+mkdir -p "$MPV_CONFIG_DIR"
+
+if [ ! -f "$MPV_CONFIG_DIR/sponsorblock.lua" ]; then
+    cp /app/share/bbs-popcorn/sponsorblock.lua "$MPV_CONFIG_DIR/sponsorblock.lua"
+fi
+
+exec python3 -m bbs_popcorn.main "$@"
